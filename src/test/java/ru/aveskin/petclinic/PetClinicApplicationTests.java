e88a65daa30c5b69a713674206827a369f57511b
aveskin <job.eskin@yandex.ru>
2025-08-25 17:28:57 +0300

    init

---
+++ src/test/java/ru/aveskin/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aveskin.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
